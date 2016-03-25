1bb87e7eb8b962d50b2874d04c4aed96142aaaa2
augisticz <augisticz@gmail.com>
2016-03-25 23:46:33 +0700

    When click img show menu (ยังไม่สมบูรณ์นะ ทำให้เขียน back-end ได้ก่อน)

--- record-management/client/components/formcomponents/patientOrgan.jsx
+++ record-management/client/components/formcomponents/patientOrgan.jsx
@@ -7,13 +7,43 @@ let marginLeft = {
     }
 
 PatientOrgan = React.createClass({
+
+  componentDidMount(){
+
+    $('.organ-model').click(function(){
+      $('.menu-organ').removeClass('hidden')
+      $('.organ-pain-type-item').removeClass('hidden')
+    }),
+
+    $('.organ-pain-type-item').click(function(){
+      $('.menu-organ').addClass('hidden')
+      $('.organ-pain-type-item').addClass('hidden')
+    })
+
+  },
+
   render(){
     return(
-      <div className="col l12">
+      <div className="col l12 organ">
         <div className="row">
-          <div style={marginLeft} className="col l6">
+          <div style={marginLeft} className="col l6 organ-model">
             <img style={imgSize} src="/images/organ.png"/>
           </div>
+        </div>
+        <div className="fixed-action-btn horizontal click-to-toggle menu-organ hidden">
+          <a className="btn-floating btn-large red">
+            <i className="large mdi-navigation-menu"></i>
+          </a>
+          <ul>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/pain.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/refered-line.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/numbless.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/subluxation.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/tighness.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/weakness.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/incision-line.png"/></a></li>
+            <li><a className="btn-floating blue organ-pain-type-item"><img style={paddingImg} src="/images/typeofpain/other.png"/></a></li>
+          </ul>
         </div>
       </div>
     )
