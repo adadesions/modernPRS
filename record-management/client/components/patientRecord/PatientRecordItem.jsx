bb4dc744de24dd73dd50a3d52caa4378dd2c16af
adadesions <ada_dev@icloud.com>
2016-03-30 18:18:05 +0700

    align code in PatientRecordItem

--- record-management/client/components/patientRecord/PatientRecordItem.jsx
+++ record-management/client/components/patientRecord/PatientRecordItem.jsx
@@ -5,16 +5,16 @@ PatientRecordItem = React.createClass({
 
 
   render() {
-    let record = this.props.record,
-        cn = record.CN,
-        contraindication = record.interview.contraindication,
-        firstname = record.information.firstname
-     return (
-       <tr>
-         <td>{cn}</td>
-         <td>{contraindication}</td>
-         <td>{firstname}</td>
-       </tr>
+      let record = this.props.record,
+          cn = record.CN,
+          contraindication = record.interview.contraindication,
+          firstname = record.information.firstname
+       return (
+         <tr>
+           <td>{cn}</td>
+           <td>{contraindication}</td>
+           <td>{firstname}</td>
+         </tr>
      )
   }
 })
